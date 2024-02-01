90dd4d38987e285887a4d51e53acb0b62f2efbca
kimhongseok <drghdtjr@naver.com>
2024-02-01 19:29:32 +0900

    Initial Commit

---
+++ menu/menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  menuApp.swift
+//  menu
+//
+//  Created by 김홍석 on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
