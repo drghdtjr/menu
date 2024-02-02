939348b16bded8122dc2708a0a4bf41635a7f371
kimhongseok <drghdtjr@naver.com>
2024-02-02 14:38:39 +0900

    24.02.02

--- menu/MenuView.swift
+++ menu/MenuView.swift
@@ -25,6 +25,8 @@ struct MenuView: View {
     
     var body: some View {
         VStack {
+            
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
